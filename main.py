c44892cb9d223365df5373bffefb68e583a1b153
saleml <salemlahlou9@gmail.com>
2018-12-06 22:13:15 -0500

    better logging normalize how logging is done in vae and iwae save images save test reconstructions

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from data.gmm_gen import GMMDataGen
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
+from torchvision.utils import save_image
 
 
 def parse_args():
@@ -58,16 +59,24 @@ def main():
         )
         dataset = datasets.MNIST('../data', train=True, download=True,
                                  transform=transform)
+
         input_dim = dataset[0][0].shape[1]
         train_loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
         model = BasicModel(input_dim, args.hidden_dim, args.hidden_layers, args.encoding_dim,
                            args.hidden_nonlinearity, args.mode)
+
+        test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('../data', train=False, transform=transform),
+            batch_size=args.batch_size, shuffle=True)
+
     elif args.dataset == 'GMM':
         train_loader = GMMDataGen(args.d, C=args.C)
         input_dim = args.d
         encoding_dim = args.C
         model = ToyModel(input_dim, args.hidden_dim, args.hidden_layers, encoding_dim,
                          args.hidden_nonlinearity, args.mode)
+
+        test_loader = None
     else:
         raise NotImplementedError("dataset doesn't exist")
 
@@ -103,12 +112,42 @@ def main():
     step = 0
     if args.dataset == 'MNIST':
         for epoch in range(args.epochs):
+            train_loss = 0.
             for batch_idx, (data, target) in enumerate(train_loader):
-                args = algo.train_step(data)
-
+                out = algo.train_step(data)
+                _, _, loss = out
+                train_loss += loss.item() * args.batch_size
                 if (batch_idx % 100) == 0:
-                    algo.visu(writer, step, args)
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data), len(train_loader.dataset),
+                               100. * batch_idx / len(train_loader),
+                               loss.item()))
+                    algo.visu(writer, step, out)
                 step += 1
+
+            print('====> Epoch: {} Average loss: {:.4f}'.format(
+                epoch, train_loss / len(train_loader.dataset)))
+
+            # save some images, and do tests
+            test_loss = 0.
+            for batch_idx, (data, target) in enumerate(test_loader):
+                _, _, recon, loss = algo.test_step(data)
+                test_loss += loss.item() * args.batch_size
+                if batch_idx == 0:
+                    n = min(data.size(0), 8)
+                    comparison = torch.cat([data.view(args.batch_size, 1, 28, 28)[:n],
+                                            recon.view(args.batch_size, 1, 28, 28)[:n]])
+                    save_image(comparison,
+                               './results/reconstruction_' + str(epoch) + '.png', nrow=n)
+
+            print('====> Test set loss: {:.4f}'.format(test_loss / len(test_loader.dataset)))
+
+            with torch.no_grad():
+                samples = model.sample(64)
+                print(samples.shape)
+                save_image(samples.view(64, 1, 28, 28),
+                           'results/sample_' + str(epoch) + '.png')
+
     if args.dataset == 'GMM':
         for step in range(5000):
             data = train_loader.next_batch(1000)
