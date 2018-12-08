b2b4f7d4b115bfde2e387d0160adc381cc0fffa2
saleml <salemlahlou9@gmail.com>
2018-12-08 17:55:15 -0500

    factorize code a bit, evaluation in basealgo superclass of algos

--- main.py
+++ main.py
@@ -12,15 +12,12 @@ from data.gmm_gen import GMMDataGen
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
-from torchvision.utils import save_image
 
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--algo", default='rws',
                         help="Algorithm to use. rws, vae, or iwae (default: rws)")
-    parser.add_argument("--variance-reduction",
-                        help="Var. reduction technique for inference network gradients var. reduction (default:None)")
 
     parser.add_argument("--hidden-dim", type=int, default=200,
                         help="Number of units in hidden layers")
@@ -73,12 +70,12 @@ def main():
 
     elif args.dataset == 'GMM':
         train_loader = GMMDataGen(args.d, C=args.C)
+        test_loader = GMMDataGen(args.d, C=args.C)
         input_dim = args.d
         encoding_dim = args.C
         model = ToyModel(input_dim, args.hidden_dim, args.hidden_layers, encoding_dim,
                          args.hidden_nonlinearity, args.mode)
 
-        test_loader = None
     else:
         raise NotImplementedError("dataset doesn't exist")
 
@@ -110,53 +107,27 @@ def main():
         raise NotImplementedError('algo not implemented')
 
     time_now = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
-    writer = tensorboardX.SummaryWriter('./logs/{}'.format(time_now))
     step = 0
+
+    writer = tensorboardX.SummaryWriter('./logs_{}/{}'.format(args.dataset, time_now))
+
     if args.dataset == 'MNIST':
+
         for epoch in range(args.epochs):
-            train_loss = 0.
+            algo.train_loss = 0.
             for batch_idx, (data, target) in enumerate(train_loader):
                 out = algo.train_step(data)
-                _, _, loss = out
-                train_loss += loss.item() * args.batch_size
-                if (batch_idx % 100) == 0:
-                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                        epoch, batch_idx * len(data), len(train_loader.dataset),
-                               100. * batch_idx / len(train_loader),
-                               loss.item()))
-                    algo.visu(writer, step, out)
+                algo.visu(writer, step, out, epoch, batch_idx, len(data), len(train_loader.dataset), args.batch_size)
                 step += 1
 
-            print('====> Epoch: {} Average loss: {:.4f}'.format(
-                epoch, train_loss / len(train_loader.dataset)))
-
-            # save some images, and do tests
-            test_loss = 0.
-            for batch_idx, (data, target) in enumerate(test_loader):
-                _, _, recon, loss = algo.test_step(data)
-                test_loss += loss.item() * args.batch_size
-                if batch_idx == 0:
-                    n = min(data.size(0), 8)
-                    comparison = torch.cat([data.view(args.batch_size, 1, 28, 28)[:n],
-                                            recon.view(args.batch_size, 1, 28, 28)[:n]])
-                    save_image(comparison,
-                               './results/reconstruction_' + str(epoch) + '.png', nrow=n)
-
-            print('====> Test set loss: {:.4f}'.format(test_loss / len(test_loader.dataset)))
-
-            with torch.no_grad():
-                samples = model.sample(64)
-                save_image(samples.view(64, 1, 28, 28),
-                           'results/sample_' + str(epoch) + '.png')
+            algo.test_log(epoch, test_loader, args.batch_size)
 
     if args.dataset == 'GMM':
-        for step in range(5000):
-            data = train_loader.next_batch(1000)
-            loss = algo.train_step(data)
-            print(loss.item(), model.pi)
-
-    # Evaluate
-    # TODO: make sure to save results (tensorboard / csv)
+        for step in range(10000):
+            data = train_loader.next_batch(args.batch_size)
+            out = algo.train_step(data)
+            algo.visu(writer, step, out, latent_proba=train_loader.latent_proba, mus=train_loader.mus)
+            algo.test_log(None, test_loader, None, step)
 
 
 if __name__ == "__main__":
