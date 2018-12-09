36a916156ac712054108fba9774d94bb0df02b02
Rim <rim.assouel@hotmail.fr>
2018-12-09 17:03:45 -0500

    exp to get the histogram of the gradients of the recog network

--- rws/exp_utils.py
+++ rws/exp_utils.py
@@ -2,18 +2,50 @@ import numpy as np
 from data import gmm_gen
 import torch
 from torch.distributions.one_hot_categorical import OneHotCategorical
+import seaborn as sns
 
 class exp:
     def __init__(self, gmm_toy, model):
         self.toy = gmm_toy
         self.model = model
         self.mode = model.mode
-        self.pi = torch.from_numpy(gmm_toy.latent_proba).float()
-        self.mus = gmm_toy.mus
-        self.cov = gmm_toy.cov
 
-    def gradients_recog(self, data, algo):
-        return
+        if gmm_toy is not None :
+            self.mus = gmm_toy.mus
+            self.cov = gmm_toy.cov
+            self.pi = torch.from_numpy(gmm_toy.latent_proba).float()
+
+    def gradients_recog(self, data, algo, algo_ = 'rws', K =5):
+        algo.K = K
+        if algo_ == 'rws':
+            grads = []
+            for i in range(50):
+                sample, mean, logvar, _, _, _=algo.forward(data[0:1,:])
+                loss_wake = algo.get_loss_q_wake_update(mean, logvar, data[0:1,:])
+                loss_sleep = algo.get_loss_q_sleep_update(mean)
+
+                loss_q = loss_wake + loss_sleep
+                self.model.zero_grad()
+                loss_q.backward()
+
+                for name, param in self.model.encoder.named_parameters():
+                    if name == '2.weight' :
+                        a = param.grad[0].item()
+                        grads.append(a)
+        elif algo_ =='iwae':
+            grads = []
+            for i in range(50):
+                sample, mean, logvar, _, _, _ = algo.forward(data[0:1, :])
+                loss = algo.get_loss(mean, logvar, data[0:1,:],)
+                self.model.zero_grad()
+                loss.backward()
+                for name, param in self.model.encoder.named_parameters():
+                    if name == '2.weight' :
+                        a = param.grad[0][0].item()
+                        grads.append(a)
+
+
+        return grads
 
     def gradients_model(self, data, algo):
         return
@@ -28,7 +60,7 @@ class exp:
 
             x_gh_sample, x_gh_mean, x_gh_sigma = self.model.decode(h)
             log_p_x_gh = torch.sum(
-                -0.5 * torch.log(x_gh_sigma ** 2) - 0.5 * (x_gh_sigma ** 2) * (data - x_gh_mean) ** 2, -1)
+                -0.5 * torch.log(x_gh_sigma ** 2) -0.5 * (1/(x_gh_sigma ** 2)) * (data - x_gh_mean) ** 2, -1)
             log_p_h = torch.sum(h * torch.log(self.pi), -1)
 
             log_p_x_h = log_p_x_gh + log_p_h
@@ -39,9 +71,12 @@ class exp:
             for i in range(C):
                 probas = torch.eye(C)[i,:]
                 h = OneHotCategorical(probas).sample((data.size()[0],))
+
                 x_gh_sample, x_gh_mean, x_gh_sigma = self.model.decode(h)
+
+                x_gh_logvar = torch.log(x_gh_sigma**2)
                 log_p_x_gh = torch.sum(
-                    -0.5 * torch.log(x_gh_sigma ** 2) - 0.5 * (x_gh_sigma ** 2) * (data - x_gh_mean) ** 2, -1)
+                    -0.5 * x_gh_logvar - 0.5 * (torch.exp(-x_gh_logvar)) * (data - x_gh_mean) ** 2, -1)
                 log_p_h = torch.sum(h * torch.log(self.pi), -1)
 
                 log_p_x_h = log_p_x_gh + log_p_h
@@ -63,8 +98,15 @@ class exp:
             diff_pi = torch.mean(torch.abs(self.pi - self.model.pi))
             diff_mu = torch.mean((self.mus -self.model.mus)**2)
 
-            return diff_pi
+            return diff_pi, diff_mu
 
         else:
             raise NotImplementedError('mode not implemented')
 
+def visu (data,algo,exp_,algo_):
+    K = [1, 5, 10, 50, 100, 1000]
+    for k in K:
+        grads = exp_.gradients_recog(data, algo, algo_, k)
+        results = np.array(grads)
+        sns.kdeplot(results, label=str(k))
+    sns.plt.show()
