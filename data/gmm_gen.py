20dc96f54eb2c4a2d9342a9f7efb00b40a972b93
saleml <salemlahlou9@gmail.com>
2018-12-10 02:39:26 -0500

    - make 1d similar to 2d - compute posteriors

--- data/gmm_gen.py
+++ data/gmm_gen.py
@@ -1,10 +1,11 @@
 import numpy as np
 from data.utils import softmax
+from scipy.stats import multivariate_normal
 import torch
 
 
 class GMMDataGen:
-    def __init__(self, d=1, C=20, std=5., radius=10., cov_diag=None):
+    def __init__(self, d=1, C=20, radius=10., cov_diag=None):
         """
         :param d: dimension
         :param C: number of classes
@@ -20,10 +21,8 @@ class GMMDataGen:
         self.latent_proba = softmax(self.theta)
 
         if d == 1:
-            # mean = 10 * latent
-            self.mus = radius * np.arange(C, dtype=float)
-            # std = fixed
-            self.std = std
+            self.mus = np.arange(C, dtype=float)
+            cov_diag = [[1.]]
 
         elif d == 2:
             if cov_diag is None:
@@ -31,33 +30,34 @@ class GMMDataGen:
             # mean = radius [cos (2pi latent / C), sin(2pi latent / C)]
             self.mus = np.array([np.cos(2 * np.pi / C * np.arange(C, dtype=float)),
                                  np.sin(2 * np.pi / C * np.arange(C, dtype=float))]).T
-            self.mus *= radius
-            self.cov = np.eye(2) * cov_diag
 
         else:
             raise NotImplementedError("d = {} Not implemented".format(d))
 
+        self.mus *= radius
+        self.cov = np.eye(d) * cov_diag
+
     def sampler(self, num_samples):
         for _ in range(num_samples):
             z = np.random.choice(np.arange(self.C), p=self.latent_proba)
-            if self.d == 1:
-                x = self.mus[z] + self.std * np.random.randn()
-            elif self.d == 2:
-                x = np.random.multivariate_normal(self.mus[z], self.cov)
-            else:
-                raise NotImplementedError("d = {} Not implemented".format(self.d))
+            x = np.random.multivariate_normal(self.mus[z], self.cov)
             yield x
 
-    def next_batch(self, batch_size):
+    def next_batch(self, batch_size, use_torch=True):
         batch = torch.zeros((batch_size, self.d))
         for i in range(batch_size):
             z = np.random.choice(np.arange(self.C), p=self.latent_proba)
-            if self.d == 1:
-                x = self.mus[z] + self.std * np.random.randn()
-            elif self.d == 2:
-                x = np.random.multivariate_normal(self.mus[z], self.cov)
-            else:
-                raise NotImplementedError("d = {} Not implemented".format(self.d))
-
-            batch[i, :] = torch.from_numpy(x)
+            x = np.random.multivariate_normal(self.mus[z], self.cov)
+            if use_torch:
+                batch[i, :] = torch.from_numpy(x)
         return batch
+
+    def get_posteriors(self, test_samples):
+        m = test_samples.shape[0]
+        post = np.zeros((m, self.C))
+        for z in range(self.C):
+            post[:, z] = multivariate_normal.pdf(test_samples, self.mus[z], self.cov)
+        post *= self.latent_proba
+        total_proba = np.sum(post, axis=1)
+        return (post.T / total_proba).T
+
