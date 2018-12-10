26f0041a810fb797c54f5aff4b63c1eb20c8e8c8
saleml <salemlahlou9@gmail.com>
2018-12-10 02:41:51 -0500

    add csv logger, scheduler, lr argument, gmm test posteriors

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import tensorboardX
 from rws import Vae, RWS, IWAE, exp
 from torch.optim import Adam
 from data.gmm_gen import GMMDataGen
-import numpy as np
-import matplotlib.pyplot as plt
+import csv
+
 import datetime
+from torch.optim.lr_scheduler import MultiStepLR
 
 
 def parse_args():
@@ -41,8 +42,13 @@ def parse_args():
     parser.add_argument('--no-RP', action='store_true', default=False,
                         help='reparametrization trick')
     parser.add_argument("--VR", default=None, help='variance reduction')
-    parser.add_argument("--d", default=2, help='dimension of data in toy gmm')
+    parser.add_argument("--d", type=int, default=2, help='dimension of data in toy gmm')
     parser.add_argument("--no-mu", action='store_true', default=False, help="learn mu in GMM")
+
+    parser.add_argument("--lr", type=float, default=1e-3, help="initial learning rate")
+
+    parser.add_argument("--milestones", nargs='*', default=[700, 1500, 2000, 3000], help='scheduler milestones')
+    parser.add_argument("--gamma", type=float, default=0.316, help='decay of lr at each milestone')
     args = parser.parse_args()
     return args
 
@@ -76,6 +82,8 @@ def main():
         encoding_dim = args.C
         model = ToyModel(input_dim, args.hidden_dim, args.hidden_layers, encoding_dim,
                          args.hidden_nonlinearity, args.mode, not args.no_mu)
+        test_set = test_loader.next_batch(1000, use_torch=False)
+        test_posteriors = test_loader.get_posteriors(test_set)
 
     else:
         raise NotImplementedError("dataset doesn't exist")
@@ -94,30 +102,47 @@ def main():
         decoder_params.append(model.pre_pi)
         if not args.no_mu:
             decoder_params.append(model.mus)
-            optimizer = Adam(list(model.parameters()) + [model.pre_pi, model.mus], lr=1e-3)
+            optimizer = Adam(list(model.parameters()) + [model.pre_pi, model.mus], lr=args.lr)
         else:
-            optimizer = Adam(list(model.parameters()) + [model.pre_pi], lr=1e-3)
+            optimizer = Adam(list(model.parameters()) + [model.pre_pi], lr=args.lr)
 
-    optim_recog = torch.optim.Adam(encoder_params, lr=1e-3)
-    optim_model = torch.optim.Adam(decoder_params, lr=1e-3)
+    optim_recog = torch.optim.Adam(encoder_params, lr=args.lr)
+    optim_model = torch.optim.Adam(decoder_params, lr=args.lr)
+
+    scheduler, scheduler_model, scheduler_recog = None, None, None
+    if len(args.milestones) > 0:
+        print('Using a scheduler')
+        scheduler = MultiStepLR(optimizer, milestones=args.milestones, gamma=args.gamma)
+        scheduler_recog = MultiStepLR(optim_recog, milestones=args.milestones, gamma=args.gamma)
+        scheduler_model = MultiStepLR(optim_model, milestones=args.milestones, gamma=args.gamma)
 
     # Train model
     if args.algo == 'rws':
-        algo = RWS(model, optim_recog, optim_model, K=args.K, mode=args.mode)
+        algo = RWS(model, optim_recog, optim_model, scheduler_recog, scheduler_model, K=args.K, mode=args.mode)
     elif args.algo == 'vae':
         algo = Vae(model, optimizer, args.mode)
     elif args.algo == 'iwae':
-        algo = IWAE(model, optimizer, args.K, args.mode, not args.no_RP, args.VR)
+        algo = IWAE(model, optimizer, scheduler, args.K, args.mode, not args.no_RP, args.VR)
     else:
         raise NotImplementedError('algo not implemented')
 
     time_now = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
     step = 0
 
-    writer = tensorboardX.SummaryWriter('./logs_{}/{}_{}_C{}_K{}_{}'.format(args.dataset, args.algo, args.VR,
-                                                                         args.C, args.K, time_now))
+    path = './logs_{}/{}_{}_C{}_K{}_{}_{}'.format(args.dataset, args.algo, args.VR,
+                                                  args.C, args.K, 'nomu' if args.no_mu else 'mu', time_now)
+    writer = tensorboardX.SummaryWriter(path)
 
-    exp_ = exp(train_loader,model)
+    exp_ = exp(train_loader, model)
+
+    csv_writer = csv.writer(open(path + '/log.csv', 'a', 1))
+    header = ['step', 'L2_pi', 'L2_mu', 'L2_posteriors']
+    if args.algo == 'rws':
+        header += ['loss_model', 'loss_q_sleep', 'loss_q_wake']
+    else:
+        header += ['loss']
+    csv_writer.writerow(header)
+
     if args.dataset == 'MNIST':
         for epoch in range(args.epochs):
             algo.train_loss = 0.
@@ -129,10 +154,11 @@ def main():
             algo.test_log(epoch, test_loader, args.batch_size)
 
     if args.dataset == 'GMM':
-        for step in range(10000):
+        for step in range(100000):
             data = train_loader.next_batch(args.batch_size)
             out = algo.train_step(data)
-            algo.visu(writer, step, out, latent_proba=train_loader.latent_proba, mus=train_loader.mus)
+            algo.visu(writer, step, out, latent_proba=train_loader.latent_proba, mus=train_loader.mus,
+                      test_set=test_set, test_posteriors=test_posteriors, csv_writer=csv_writer)
             algo.test_log(None, test_loader, None, step)
 
 
